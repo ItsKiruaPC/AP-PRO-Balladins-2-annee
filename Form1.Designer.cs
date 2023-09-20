8f8f06997f0d0ed2844b9a2c7fdc2c44ffcc0d6b
ItsKiruaPC <adrien.fevre31@gmail.com>
2023-09-20 11:08:24 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AP_PRO_Balladins
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnconnexion = new System.Windows.Forms.Button();
+            this.txtmdp = new System.Windows.Forms.TextBox();
+            this.txtnomuser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblinfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 39);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Se Connecter";
+            // 
+            // btnconnexion
+            // 
+            this.btnconnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconnexion.Location = new System.Drawing.Point(187, 350);
+            this.btnconnexion.Name = "btnconnexion";
+            this.btnconnexion.Size = new System.Drawing.Size(128, 46);
+            this.btnconnexion.TabIndex = 11;
+            this.btnconnexion.Text = "Connexion";
+            this.btnconnexion.UseVisualStyleBackColor = true;
+            this.btnconnexion.Click += new System.EventHandler(this.btnconnexion_Click);
+            // 
+            // txtmdp
+            // 
+            this.txtmdp.Location = new System.Drawing.Point(142, 222);
+            this.txtmdp.Name = "txtmdp";
+            this.txtmdp.Size = new System.Drawing.Size(238, 22);
+            this.txtmdp.TabIndex = 10;
+            // 
+            // txtnomuser
+            // 
+            this.txtnomuser.Location = new System.Drawing.Point(142, 140);
+            this.txtnomuser.Name = "txtnomuser";
+            this.txtnomuser.Size = new System.Drawing.Size(238, 22);
+            this.txtnomuser.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Login";
+            // 
+            // lblinfo
+            // 
+            this.lblinfo.AutoSize = true;
+            this.lblinfo.Location = new System.Drawing.Point(107, 276);
+            this.lblinfo.Name = "lblinfo";
+            this.lblinfo.Size = new System.Drawing.Size(0, 16);
+            this.lblinfo.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Controls.Add(this.lblinfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnconnexion);
+            this.Controls.Add(this.txtmdp);
+            this.Controls.Add(this.txtnomuser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnconnexion;
+        private System.Windows.Forms.TextBox txtmdp;
+        private System.Windows.Forms.TextBox txtnomuser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblinfo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AP_PRO_Balladins
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconnexion_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtmdp.Text) || string.IsNullOrEmpty(txtnomuser.Text))
+            {
+                lblinfo.Text="Veuillez saisir votre mot de passe et votre utilisateur";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
